500a0630a276b4008d46fbe43211b8112477b631
ProjetoSistema <kauemroberto@gmail.com>
2024-08-16 10:58:42 -0300

    dispiar

--- ProjetoSistema/Telas/TelaInicial.xaml.cs
+++ ProjetoSistema/Telas/TelaInicial.xaml.cs
@@ -9,5 +9,26 @@ namespace ProjetoSistema
         {
             InitializeComponent();
         }
+
+        private void EntrarTelaPedido(object sender, EventArgs args)
+    {
+        if (Application.Current != null)
+        Application.Current.MainPage = new TelaPedidos();
+    }
+     private void EntrarTelaMateriaPrima(object sender, EventArgs args)
+    {
+        if (Application.Current != null)
+        Application.Current.MainPage = new TelaMateriaPrima();
+    }
+    private void EntrarTelaSetores(object sender, EventArgs args)
+    {
+        if (Application.Current != null)
+        Application.Current.MainPage = new TelaSetores();
+    }
+    private void EntrarTelaFornecedores(object sender, EventArgs args)
+    {
+        if (Application.Current != null)
+        Application.Current.MainPage = new TelaFornecedores();
+    }
     }
 }
