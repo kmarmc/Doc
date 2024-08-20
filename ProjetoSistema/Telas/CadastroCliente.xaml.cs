4100732b8655b170f95d8b85f761beda3af54fb4
Doc <kauemroberto@gmail.com>
2024-08-20 11:20:43 -0300

    horrivi

--- ProjetoSistema/Telas/CadastroCliente.xaml.cs
+++ ProjetoSistema/Telas/CadastroCliente.xaml.cs
@@ -1,38 +1,81 @@
 using System;
+using Controles;
 using Microsoft.Maui.Controls;
+using Modelos;
 
 namespace ProjetoSistema
 {
     public partial class CadastroCliente : ContentPage
     {
+        public Cliente cliente { get; set; }
+        ControleCliente controleCliente = new ControleCliente();
+
         public CadastroCliente()
         {
             InitializeComponent();
         }
 
-        // Event handlers can be added here for button clicks if needed.
+   
 
-        async void CadClicked(object sender, EventArgs args)
-        {
-            var resp = await DisplayAlert("Cadastro", "Você tem certeza que deseja cadastrar?", "sim", "não");
-            if (resp)
-            {
-
-            }
-        }
-        async void CancelClicked(object sender, EventArgs args)
-        {
-            var resp = await DisplayAlert("Cadastro", "Você tem certeza que deseja cancelar?", "sim", "não");
-            if (resp)
-            {
-
-            }
-        
-        }
         private void Voltar(object sender, EventArgs args)
-    {
-        if (Application.Current != null)
-        Application.Current.MainPage = new TelaInicial();
-    }
+        {
+            if (Application.Current != null)
+                Application.Current.MainPage = new TelaInicial();
+        }
+        private async void DeletarCliente(object sender, EventArgs e)
+        {
+            if (cliente == null || cliente.Id < 1)
+                await DisplayAlert("Erro", "Nenhum cliente para excluir", "ok");
+            else if (await DisplayAlert("Deletar", "Você tem certeza que deseja deletar?", "Sim", "Não"))
+            {
+                controleCliente.Apagar(cliente.Id);
+                Application.Current.MainPage = new TelaCliente();
+            }
+        }
+        protected async void SalvarCliente(object sender, EventArgs e)
+        {
+            if (await VerificarDados())
+            {
+                var cliente = new Modelos.Cliente();
+            if (!String.IsNullOrEmpty(IdLabel.Text))
+                cliente.Id = int.Parse(IdLabel.Text);
+            else
+                cliente.Id = 0;
+            cliente.Nome = NomeEntry.Text;
+            cliente.Endereco = EnderecoEntry.Text;
+            cliente.Email = EmailEntry.Text;
+            cliente.Telefone = TelefoneEntry.Text;
+
+            controleCliente.CriarOuAtualizar(cliente);
+            await DisplayAlert("Salvar", "Dados salvos com sucesso!", "OK");
+        }
+         private async Task<bool> VerificarDados()
+        {
+
+            if (String.IsNullOrEmpty(NomeEntry.Text))
+            {
+                await DisplayAlert("Cadastrar", "O campo Nome é obrigatório", "OK");
+                return false;
+            }
+            
+            else if (String.IsNullOrEmpty(EnderecoEntry.Text))
+            {
+                await DisplayAlert("Cadastrar", "O campo Sobrenome é obrigatório", "OK");
+                return false;
+            }
+            else if (String.IsNullOrEmpty(EmailEntry.Text))
+            {
+                await DisplayAlert("Cadastrar", "O campo Telefone é obrigatório", "OK");
+                return false;
+            }
+            else if (String.IsNullOrEmpty(TelefoneEntry.Text))
+            {
+                await DisplayAlert("Cadastrar", "O campo Telefone é obrigatório", "OK");
+                return false;
+            }
+            else
+                return true;
+        }
+        }
     }
 }
